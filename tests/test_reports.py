7519ae9791dd1456475c2bd50799b9a139dbcf5f
ВашеИмя <your_email@example.com>
2026-06-10 11:13:20 +0300

    Сделал тест по test_spending_by_category(reports)

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -1,5 +1,5 @@
 import pytest
-
+import pandas as pd
 from unittest.mock import patch, Mock
 from src.reports import get_transactions_in_excel_file, spending_by_category
 
@@ -11,3 +11,26 @@ def test_get_transactions_in_excel_file(mock_join, mock_read_excel, operations_d
     mock_read_excel.return_value = operations_dataframe
     result = get_transactions_in_excel_file()
     assert result.to_dict() == operations_dataframe.to_dict()
+
+
+@pytest.mark.parametrize(
+    'transactions_before, transactions_after', [
+        (pd.DataFrame({
+        'Дата операции': ['01.07.2018 12:49:53',
+                          '11.07.2018 12:48:53',
+                          '12.07.2018 16:49:53',
+                          '02.07.2018 12:49:53'],
+        'Категория': ['Связь',
+                      'Связь',
+                      'Различные товары',
+                      'Транспорт']}),
+
+        pd.DataFrame({
+                        'Дата операции': ['01.07.2018 12:49:53',
+                                          '11.07.2018 12:48:53'],
+                        'Категория': ['Связь', 'Связь']}))])
+def test_spending_by_category(transactions_before, transactions_after):
+    result = spending_by_category(transactions=transactions_before,
+                                  date='15.07.2018 16:49:53', category='Связь')
+    assert (result[['Категория']].to_dict() ==
+            transactions_after[['Категория']].to_dict())
